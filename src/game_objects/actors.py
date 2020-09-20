6e6a6a63703529ee96157e688ea09c64c0634f35
0Nikola0 <nikoladesigns3@gmail.com>
2020-09-20 17:23:16 +0200

    Animations try

--- src/game_objects/actors.py
+++ src/game_objects/actors.py
@@ -9,10 +9,16 @@ class ActorAdult(pygame.sprite.Sprite):
 
         sh = {key: SpriteSheet(value) for key, value in sprite_sheets.items()}
 
-        self.image = sh["IDLE"].get_image(0)
-        pygame.transform.scale(self.image, s.ADULT_ACTOR_SIZE)
-        self.rect = self.image.get_rect(topleft=pos)
+        self.images_idle = []
+        for i in range(4):
+            self.images_idle.append(sh["IDLE"].get_image(i))
 
+        self.image = self.images_idle[0]
+        pygame.transform.scale(self.image, s.ADULT_ACTOR_SIZE)
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.anim_type = 0
+        self.state = "idle"
         self.vel = 5
 
     def move_left(self):
@@ -20,3 +26,14 @@ class ActorAdult(pygame.sprite.Sprite):
 
     def move_right(self):
         self.rect.x += self.vel
+
+    def move_up(self):
+        self.rect.y -= self.vel
+
+    def move_down(self):
+        self.rect.y += self.vel
+
+    def update(self):
+        self.image = self.images_idle[self.anim_type]
+        pygame.time.delay(100)
+        self.anim_type = (self.anim_type + 1) if self.anim_type < 3 else 0
