6993ed7537d0a7f7702e216fc45446e7cd951b61
0Nikola0 <nikoladesigns3@gmail.com>
2020-09-21 18:37:30 +0200

    Added constants for GUI

--- src/settings.py
+++ src/settings.py
@@ -21,5 +21,6 @@ SPRITES_SHEET_SPRITE_SIZE = (48, 48)
 
 # GUI
 # HELLO_BUTTON_POS, HELLO_BUTTON_SIZE = (50, 50), (100, 50)
+# ### Need these to set limit to where the player can move
 PANEL_POS, PANEL_SIZE = (600, 0), (200, 400)
 EVENT_DESC_POS, EVENT_DES_SIZE = (0, SCREEN_HEIGHT - 200), (SCREEN_WIDTH, 200)
