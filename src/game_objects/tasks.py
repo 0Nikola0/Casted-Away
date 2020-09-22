45d6fe384cbc4c88b6a82f38dbf3cb9154de22dc
0Nikola0 <nikoladesigns3@gmail.com>
2020-09-22 22:32:12 +0200

    Added differents methods for different tasks

--- src/game_objects/tasks.py
+++ src/game_objects/tasks.py
@@ -3,13 +3,31 @@ from src import settings as s
 
 
 class Task(pygame.sprite.Sprite):
-    def __init__(self, name, pos, size):
+    def __init__(self, task_id: int,  increasement: float, pos, size):
         super(Task, self).__init__()
-        self.name = name
+        """
+        Harvesting = ID: 0
+        Getting water = ID: 1
+        Sleep / Eat / Drink = ID: 2
+        """
+        self.task_id = task_id
+        self.increasement = increasement    # How much you gain from doing the task per loop
         self.rect = pygame.Rect(pos, size)
 
     def draw(self, surface):
         pygame.draw.rect(surface, s.BLACK, self.rect)
 
     def do_task(self):
-        pass
+        # Checks which ID the task is assigned and calls the corresponding function
+        {0: self.harvest, 1: self.get_water, 2: self.increase_actor_stats}.get(self.task_id)()
+
+    def harvest(self):
+        s.FOOD_SUPPLY += self.increasement
+
+    def get_water(self):
+        s.WATER_SUPPLY += self.increasement
+
+    # Same method can be used to increase each of the actor's stats eg Food status, Sleep status..
+    def increase_actor_stats(self, current):
+        current += self.increasement
+        return current
