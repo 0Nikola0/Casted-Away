b12307fa274b30f69709d50d16aaf386f06aa194
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2020-09-24 15:02:05 -0400

    REFACTOR: TestScene and SelectionBox
    Merged TestScene code into GameScene, TestScene now simply inherits
    GameScene, though there is some work to be done with layers.
    
    SelectionBox can be bound to an actor and kind of works.
    
    Also moved EmptySprite to a separate file.

--- src/state.py
+++ src/state.py
@@ -3,14 +3,7 @@ import pygame
 import src.settings as s
 
 from src.game_objects.gui import console_print_event
-
-
-class EmptySprite(pygame.sprite.Sprite):
-    """Used to receive updates from main_loop, kind of hacky"""
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((0,0))
-        self.rect = pygame.Rect((0,0,0,0))
+from src.game_objects.empty_sprite import EmptySprite
 
 
 class State(EmptySprite):
