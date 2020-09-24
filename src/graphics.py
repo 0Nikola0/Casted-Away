5d3b5659f25f7c19afc79b9eba37bb73cb6f757a
Denis Snagovskiy <denis.snagovskiy@list.ru>
2020-09-24 09:03:31 +0300

    Update graphics.py SpriteSheet docstring

--- src/graphics.py
+++ src/graphics.py
@@ -1,9 +1,3 @@
-"""
-usage:
-    sheet = spritesheet.SpriteSheet()
-    image = sheet.get_image('player_idle', scale=(x,y))
-"""
-
 import pygame
 import src.settings as s
 
@@ -13,6 +7,32 @@ NAME = {i: (sx * i + 1, 0, *size) for i in range(99)}
 
 
 class SpriteSheet:
+    """Load sprite images from a sprite sheets
+
+    usage:
+
+    sheet = SpriteSheet(s.OLD_WOMAN_SPRITE_SHEETS["IDLE"])  # get only 1 sprite sheet (with IDLE image set)
+    self.image = sh.get_image(0)  # get first image
+
+    usage 2:
+
+    sh = sh = {key: SpriteSheet(value) for key, value in s.OLD_WOMAN_SPRITE_SHEETS.items()}  # get all sprite sheets
+    animation_length = 4  # number of images in 1 sheet
+
+    self.animation_sets = []  # list of lists of all images / for select animation set
+        for sh in shs:
+            image_set = []  # list of all images in 1 sprite sheet
+            for i in range(animation_length):
+                image_set.append(shs[sh].get_image(i))
+            self.animation_sets.append(image_set)
+
+    self.state = {state_name: i for i, state_name in enumerate(shs)}
+
+    self.current_state = self.state["IDLE]
+    self.current_frame = 0
+
+    self.image = self.animation_sets[self.current_state][self.current_frame]
+    """
     def __init__(self, sheet_path):
         self.source_image = pygame.image.load(sheet_path).convert()
         self.source_image.set_colorkey(s.WHITE)
